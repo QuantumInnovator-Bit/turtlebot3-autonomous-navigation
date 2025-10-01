c1f8f01ebb808d16f09e141b8aa130519416dc4a
Satyam Raj <satyamraj.quantumbit@zohomail.in>
2025-10-02 02:21:10 +0530

    Add turtlebot3_autonomous_nav/launch/gazebo_sim.launch.py

---
+++ turtlebot3_autonomous_nav/launch/gazebo_sim.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_file_name = 'turtlebot3_world.world'
+    
+    turtlebot3_gazebo_dir = get_package_share_directory('turtlebot3_gazebo')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    
+    world = os.path.join(
+        turtlebot3_gazebo_dir,
+        'worlds',
+        world_file_name
+    )
+    
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+    
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+    
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo_dir, 'launch', 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+    
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(turtlebot3_gazebo_dir, 'launch', 'spawn_turtlebot3.launch.py')
+        )
+    )
+    
+    ld = LaunchDescription()
+    
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+    
+    return ld
